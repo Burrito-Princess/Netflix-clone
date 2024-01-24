a81636b567793dd371bd1c9bbdcb7251b5cfddfa
Pz81 <220739@student.glu.nl>
2024-01-24 09:16:25 +0100

    de opset

---
+++ src/home/Home.jsx
@@ -0,0 +1,13 @@
+import { AcUnit } from "@material-ui/icons"
+import "./home.scss"
+
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <AcUnit/>
+        </div>
+    )
+}
+
+export default Home
